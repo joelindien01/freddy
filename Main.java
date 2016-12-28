4927b6789df90202c232ad83caa07684a390d7c1
joelindien01 <adigo.axel@gmail.com>
2016-12-28 17:57:01 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,40 @@
+package com.creteil.iut.classmanagement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private List<Composant> stockList;
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    private int[] createNreference(int nbreDeReference){
+        int[] toReturn = new int[nbreDeReference];
+        return new int[nbreDeReference];
+    }
+
+    private void removeComposantFromStock(int composantToRemoveReference){
+        List<Composant> toRemove = new ArrayList<>();
+        for(Composant composant: stockList){
+            if(composant.getReference() == composantToRemoveReference){
+                toRemove.add(composant);
+            }
+        }
+        stockList.removeAll(toRemove);
+    }
+
+    private void addComposantToStock(Composant composant){
+        stockList.add(composant);
+    }
+
+    private void editCompanyState(){
+        System.out.println("list des composants en stock: " + stockList.size());
+    }
+
+    private boolean hasDifferentReference(Composant composant1, Composant composant2){
+        return composant1.getReference() != composant2.getReference();
+    }
+}
